25fc211b38ba5c3fdb30d5edc3d29330012798c8
Denis Angell <dangell@transia.co>
2024-04-06 09:14:10 +0200

    Update payment.test.ts

--- test/integration/payment.test.ts
+++ test/integration/payment.test.ts
@@ -1,25 +1,36 @@
-// xrpl-helpers
 import {
   LedgerTestContext,
-  setupClient,
-  teardownClient,
+  setupLedger,
+  teardownLedger,
   testTransaction,
 } from '../../dist/npm/src'
+// xrpl-helpers
+import {
+  XrplIntegrationTestContext,
+  setupClient,
+  teardownClient,
+} from '@transia/hooks-toolkit/dist/npm/src/libs/xrpl-helpers'
 
 // Router: ACCEPT: success
 
-describe('Invoke', () => {
-  let testContext: LedgerTestContext
+describe('Payment', () => {
+  let ledgerContext: LedgerTestContext
+  let testContext: XrplIntegrationTestContext
 
   beforeAll(async () => {
     testContext = await setupClient()
+    ledgerContext = await setupLedger(testContext)
   })
   afterAll(async () => {
     teardownClient(testContext)
+    teardownLedger(ledgerContext)
   })
 
-  it('invoke - all', async () => {
-    console.log('TESTING')
-    await testTransaction(testContext, 'test/fixtures/01-payment/01-basic.json')
+  it('payment - basic', async () => {
+    await testTransaction(
+      testContext,
+      ledgerContext,
+      'test/fixtures/01-payment/01-basic.json'
+    )
   })
 })

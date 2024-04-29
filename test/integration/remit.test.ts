65fb2a6d95f0c139df4b05ad2281bfdfad054283
Denis Angell <dangell@transia.co>
2024-04-29 11:15:24 +0200

    add remit

--- test/integration/remit.test.ts
+++ test/integration/remit.test.ts
@@ -53,18 +53,18 @@ describe('Remit', () => {
   //   expect(response.result.engine_result).toMatch('tesSUCCESS')
   //   await close(testContext.client)
   // })
-  it('remit - transfer token', async () => {
-    const filepath = 'test/fixtures/28-remit/05-transfer-token.json'
-    const txBlob = await testTransaction(testContext, ledgerContext, filepath)
-    const response = await testContext.client.submit(txBlob)
-    expect(response.result.engine_result).toMatch('tesSUCCESS')
-    await close(testContext.client)
-  })
-  // it('remit - all', async () => {
-  //   const filepath = 'test/fixtures/28-remit/06-all.json'
+  // it('remit - transfer token', async () => {
+  //   const filepath = 'test/fixtures/28-remit/05-transfer-token.json'
   //   const txBlob = await testTransaction(testContext, ledgerContext, filepath)
   //   const response = await testContext.client.submit(txBlob)
   //   expect(response.result.engine_result).toMatch('tesSUCCESS')
   //   await close(testContext.client)
   // })
+  it('remit - all', async () => {
+    const filepath = 'test/fixtures/28-remit/06-all.json'
+    const txBlob = await testTransaction(testContext, ledgerContext, filepath)
+    const response = await testContext.client.submit(txBlob)
+    expect(response.result.engine_result).toMatch('tesSUCCESS')
+    await close(testContext.client)
+  })
 })
